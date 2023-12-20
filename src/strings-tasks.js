9be6adf9da8390ca9d2f398a75451d3a6c6d2a37
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2023-12-21 01:07:04 +0300

    feat: add next ten solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -308,8 +308,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  let j = 0;
+  const vowels = 'aeiouyAEIOUY';
+  str.split('').forEach((i) => {
+    if (vowels.includes(i)) {
+      j += 1;
+    }
+  });
+  return j;
 }
 
 /**
@@ -325,8 +332,29 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  if (
+    str
+      .replaceAll(',', '')
+      .replaceAll(',', '')
+      .replaceAll('!', '')
+      .replaceAll('?', '')
+      .replaceAll(' ', '')
+      .toLowerCase() ===
+    str
+      .toLowerCase()
+      .split('')
+      .reverse()
+      .join('')
+      .replaceAll(',', '')
+      .replaceAll(',', '')
+      .replaceAll('!', '')
+      .replaceAll('?', '')
+      .replaceAll(' ', '')
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -341,8 +369,13 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  let maxWord = '';
+  sentence.split(' ').forEach((i) => {
+    if (i.length > maxWord.length) maxWord = i;
+  });
+  // console.log(`this max word: ${maxWord}`)
+  return maxWord;
 }
 
 /**
@@ -355,8 +388,8 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str.split('').reverse().join('').split(' ').reverse().join(' ');
 }
 
 /**
@@ -370,8 +403,16 @@ function reverseWords(/* str */) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let sentence = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const newWords =
+      str[i] === str[i].toLowerCase()
+        ? str[i].toUpperCase()
+        : str[i].toLowerCase();
+    sentence += newWords;
+  }
+  return sentence;
 }
 
 /**
@@ -387,8 +428,8 @@ function invertCase(/* str */) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -401,8 +442,8 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.slice(7, value.length - 1);
 }
 
 /**
@@ -416,8 +457,8 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.replace('<', '').replace('>', '');
 }
 
 /**
@@ -435,8 +476,8 @@ function unbracketTag(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
